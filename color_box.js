d001a9e652485d090bcee817ece9257f2d8dbc09
Preeti Kale <kalepriti21@navgurukul.org>
2023-06-15 12:16:44 +0530

    First commit

---
+++ color_box.js
@@ -0,0 +1,28 @@
+
+
+const Box=document.getElementById("Box");
+const red_button=document.getElementById("red");
+red_button.onclick=function(){
+	Box.style.backgroundColor="red";
+}
+const yellow_button=document.getElementById("yellow");
+yellow_button.onclick=function(){
+	Box.style.backgroundColor="yellow";
+}
+const green_button=document.getElementById("green");
+green_button.onclick=function(){
+	Box.style.backgroundColor="green";
+};
+const my=document.getElementById("mypara")
+const click=document.getElementById("click");
+click.onclick=function(){
+	my.innerHTML="Hellow";
+	}
+
+
+
+
+
+
+
+
